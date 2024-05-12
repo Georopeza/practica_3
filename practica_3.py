5ab7788146a2ff9ac791068f8445a7a5f078c673
edvillalobos <51093557+edvilllalobos@users.noreply.github.com>
2024-05-12 00:46:06 -0400

    comentarios y cambios ligeros en los endpoints

--- practica_3.py
+++ practica_3.py
@@ -11,28 +11,33 @@ class User(BaseModel):
     
 db: Dict[int, list] = {}
 
+# retorno de msg de prueba 
 @app.get("/")
 async def welcome():
     return {"message":"Computacion en la nube prueba"}
 
+# prueba de status 
 @app.get("/status/")
 def status():
     return {"message": "Pong"}
 
+# lista de objetos 
 @app.get("/directories/")
 def listado_objetos():
     return db
 
+# Insertar nombre e email del usuario
 @app.post("/directories/")
 def set_product(user: User) -> User:
     print("Ingrese el nombre")
-    new_name=input()
+    new_name = input()
     print("Ingrese el email")
-    new_email=input()
-    user=new_name,new_email
+    new_email = input()
+    user = new_name,new_email
     db.append(user)
     return user
 
+# retorna una db por el id del usuario
 @app.get("/directories/{id}")
 def find_by_id(id: int)-> User | None:
     user_tuple = db.get(id)
@@ -41,13 +46,14 @@ def find_by_id(id: int)-> User | None:
         return  User(id=id, name=user_tuple[id], emails=user_tuple[id])
     return None
 
+# nuevo email y nombre para el usuario
 @app.put("/directories/{id}")
 def edit_by_id(id: int, user:User)-> User | None:
     print("Ingrese el nuevo nombre")
-    new_name=input()
+    new_name = input()
     print("Ingrese el nuevo email")
-    new_email=input()
-    user=new_name,new_email
+    new_email = input()
+    user = new_name,new_email
     user_tuple = db.get(id)
     
     if user_tuple:
@@ -55,13 +61,14 @@ def edit_by_id(id: int, user:User)-> User | None:
         return user
     return None
 
+# nuevo email y nombre para el usuario
 @app.patch("/directories/{id}")
 def edit_temp_by_id(id: int, user:User)-> User | None:
     print("Ingrese el nuevo nombre")
-    new_name=input()
+    new_name = input()
     print("Ingrese el nuevo email")
-    new_email=input()
-    user=new_name,new_email
+    new_email = input()
+    user = new_name,new_email
     user_tuple = db.get(id)
     
     if user_tuple:
